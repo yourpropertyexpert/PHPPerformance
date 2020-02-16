24175441c83175a5cb24b8b583f6de605536d3b2
Mark Harrison <markharrison@Marks-MacBook-Pro-2.local>
2020-02-16 14:48:37 +0000

    Demo second (non-functional)

--- frontend/index.php
+++ frontend/index.php
@@ -11,6 +11,8 @@ echo '<p>The "something" is generating a set of random numbers, and summing them
 echo "<p>Running ".ITERATIONS." iterations of each loop. You can change this number be modifying the
 constant at the top of frontend/index.php</p>";
 
+echo '<table><thead><th>Method</th><th>Time</th><th>Factor</th></thead><tbody>';
+
 // The simple loop
 $starttime = microtime(true);
 $i = 0;
@@ -20,4 +22,10 @@ while ($i < ITERATIONS) {
     $i++;
 }
 $totalLoop = round(microtime(true) - $starttime, 5);
-echo "<p>Running a simple loop took $totalLoop ms</p>";
+echo "<tr><td>Running a simple loop</td><td>$totalLoop ms</td><td>1</td></tr>";
+
+$secondLoop = $totalLoop * 2;
+echo "<tr><td>Running a difficult loop</td><td>$secondLoop ms</td><td>2</td></tr>";
+
+
+echo '</tbody></table>';
