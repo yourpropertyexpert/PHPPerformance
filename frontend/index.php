9c54db4873fdbb6e7696ead696047a8edf5ee07f
Mark Harrison <yourpropertyexpert@gmail.com>
2020-08-14 17:16:19 +0100

    PSR-12 (attempt 2)

--- frontend/index.php
+++ frontend/index.php
@@ -18,7 +18,8 @@ echo <<< PAGE_TOP
 <head>
   <script src="https://code.highcharts.com/highcharts.js"></script>
   <link rel="stylesheet"
-    href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
     crossorigin="anonymous">
 
 </head>
