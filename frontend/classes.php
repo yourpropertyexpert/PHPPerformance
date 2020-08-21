2619e8b1082bc9fa7f8a1f7b2358fba67c2a2617
Mark Harrison <yourpropertyexpert@gmail.com>
2020-08-21 14:17:05 +0100

    New Way: SQLLite single query

--- frontend/classes.php
+++ frontend/classes.php
@@ -217,6 +217,18 @@ class Demo
         return $n;
     }
 
+    public function getNFromSQLiteInOneGo($count)
+    {
+        $i = 0;
+        $n = 0;
+        $resultarray = $this->sqlite->query("SELECT Val FROM $this->sqlTable LIMIT $count");
+        while ($thisone = $resultarray->fetchArray()) {
+            error_log("got one - ".$thisone[0]);
+            $n += $thisone[0];
+        }
+        return $n;
+    }
+
     private function connectDBs()
     {
         // Make all the external connections (not just databases) - this is called to construct
